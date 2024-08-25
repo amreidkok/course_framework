559fe121ae710cd8294fbc05ce2df6471781d2dd
amreidkok <eng.amrhassan1013@gmail.com>
2024-08-25 19:29:17 +0300

    firstcommit

--- src/main/java/HelperPages/Login.java
+++ src/main/java/HelperPages/Login.java
@@ -2,7 +2,7 @@ package HelperPages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+
 
 public class Login {
     public static WebDriver driver = WebDriverFactory.getDriver();
--- src/main/java/HelperPages/WebDriverFactory.java
+++ src/main/java/HelperPages/WebDriverFactory.java
@@ -4,6 +4,7 @@ package HelperPages;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+
 public class WebDriverFactory {
     public static WebDriver cdriver;
     public static WebDriver getDriver() {
